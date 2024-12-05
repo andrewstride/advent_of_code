ca3af9daafc01810b424848d42464a291c18c088
Andrew Stride <andrewjonathanstride@gmail.com>
2024-12-05 14:45:48 +0000

    complete day4 pt2

--- src_2024/day_004/day_04.py
+++ src_2024/day_004/day_04.py
@@ -29,3 +29,29 @@ def wordsearch(input) -> int:
             count += [diag_ne, diag_se, hor, ver].count("XMAS")
             count += [diag_ne, diag_se, hor, ver].count("SAMX")
     return count
+
+
+def x_mas(input) -> int:
+    """finds number of times 'MAS' appears in X-shape
+
+    Args:
+        input (srt): wordsearch
+
+    Returns:
+        answer (int)"""
+    count = 0
+    lines = [line.strip() for line in input.split("\n")]
+    height = len(lines)
+    width = len(lines[0])
+
+    for y in range(height):
+        for x in range(width):
+            diag_se = ""
+            diag_ne = ""
+            if (y + 3) <= height and (x + 3) <= width:
+                diag_se = "".join([lines[y + i][x + i] for i in range(3)])
+            if (y + 3) <= height and (x + 3) <= width:
+                diag_ne = "".join([lines[y + (2 - i)][x + i] for i in range(3)])
+            if diag_ne in ["MAS", "SAM"] and diag_se in ["MAS", "SAM"]:
+                count += 1
+    return count
