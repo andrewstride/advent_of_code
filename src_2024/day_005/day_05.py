3601e7b4b757c94dfc86395749e01901b4816368
Andrew Stride <andrewjonathanstride@gmail.com>
2024-12-05 22:47:59 +0000

    complete pt2

--- src_2024/day_005/day_05.py
+++ src_2024/day_005/day_05.py
@@ -1,4 +1,4 @@
-def print_queue(print_rules) -> int:
+def print_queue(print_rules, pt2=False) -> int:
     """Sorts print queue based on given rules
 
     Args:
@@ -11,6 +11,7 @@ def print_queue(print_rules) -> int:
     rules = [[int(n) for n in rule.split("|")] for rule in rules.split("\n")]
     updates = [[int(n) for n in update.split(",")] for update in updates.split("\n")]
     valid_updates = []
+    corrected_updates = []
     for update in updates:
         include = True
         for i in range(len(update)):
@@ -26,7 +27,23 @@ def print_queue(print_rules) -> int:
                         include = False
         if include:
             valid_updates.append(update)
+        while not include:
+            for i in range(len(update)):
+                after = [update[n] for n in range(i + 1, len(update))]
+                for n in after:
+                    for rule in rules:
+                        if rule == [n, update[i]]:
+                            j = update.index(n)
+                            update[i], update[j] = n, update[i]
+                            continue
+            corrected_updates.append(update)
+            include = True
     middle_total = 0
+    if pt2:
+        for update in corrected_updates:
+            middle = int((len(update) - 1) / 2)
+            middle_total += update[middle]
+        return middle_total
     for update in valid_updates:
         middle = int((len(update) - 1) / 2)
         middle_total += update[middle]
